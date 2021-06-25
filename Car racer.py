e3f1a938c093bd509857059ee27cb4ec87e1d071
Charley Cates <CCates@stu.kerikerihigh.ac.nz>
2021-06-25 12:15:16 +1200

    started quiz quest

--- Car racer.py
+++ Car racer.py
@@ -13,28 +13,35 @@ def get_integer_input(message, error, low, high):
         except ValueError:
             print(error)
 
+def roll_two_dice():
+    return roll_dice() + roll_dice()
 
 def roll_dice():
     return random.randint(1, 6)
 
+cars = []
+for i in range (12):
+    cars.append(0)
 
-car_choice = get_integer_input('Choose a car from 1 to 6 \n', 'Please choose a car from 1 to 6', 0, 7)
+car_choice = get_integer_input(f'Choose a car from 1 to {len(cars)} \n', f'Please choose a car from 1 to {len(cars)}', 0, len(cars) + 1)
 
 
 race_distance = get_integer_input('pick a race distance between 5 and 15 \n', 'Please enter an amount between 5 and 15', 4, 16)
 
 
-cars = [0, 0, 0, 0, 0, 0]
 
 
-while max(cars) < race_distance:
+while len([i for i in cars if i >= 15]) < 3:
     for i, car in enumerate(cars):
-        print ('-'*car + 'o-o')
+        if i == car_choice - 1 and car < 15:
+            print('\033[1;32;40m' + '-' * car + 'o=o')
+        else:
+            print('\033[1;31;40m' + '-' * car + 'o=o')
     input('push enter to continue')
     os.system('cls')
-    cars[roll_dice()-1] += 1
+    cars[roll_two_dice()-1] += 1
 
-
+sorted_cars = sorted(cars)
 winning_car = cars.index(max(cars)) + 1
 print(f"Car {winning_car} won!")
 
